61dfdab5aac65daaaa5c48df1ac2bf585c54512e
SARTHAK JAIN <168121818+jain-sarthak007@users.noreply.github.com>
2024-08-05 20:16:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,6 @@
 import subprocess
-import sys
 
-# def install(package):
-#     """Install a package using pip."""
-#     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
-
-# # Install packages individually
-# print("Installing streamlit...")
-# install("streamlit")
-
-# print("Installing scikit-learn...")
-# install("scikit-learn")
+install("scikit-learn")
 
 
 import numpy as np
