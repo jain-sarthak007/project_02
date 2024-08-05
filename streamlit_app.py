6fb68c11317435f361b7986a8f4605db85c30338
SARTHAK JAIN <168121818+jain-sarthak007@users.noreply.github.com>
2024-08-05 20:07:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,12 +13,12 @@ print("Installing scikit-learn...")
 install("scikit-learn")
 
 
-    import numpy as np
-    import pandas as pd
-    from sklearn.model_selection import train_test_split
-    from sklearn.linear_model import LinearRegression
-    from sklearn import metrics
-    import streamlit as st
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
+import streamlit as st
 
 
 # Title
